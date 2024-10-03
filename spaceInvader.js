078a0385ab8d340d9a0cfce4bb0a4a74e08f7b3d
Xander Dubois <xander.dubois@student.ehb.be>
2024-10-03 10:15:17 +0200

    Made my profile picture

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,27 @@
-"use strict";
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+DrawBackground();
+function DrawBackground() {
+  context.fillStyle = "black";
+  context.beginPath();
+  context.rect(0, 0, 300, 300);
+  context.stroke();
+  context.fill();
+}
 
-alert("Draw your space invader here");
+DrawLogo();
+function DrawLogo() {
+  context.fillStyle = "#84d2cf";
+
+  context.beginPath();
+  context.rect(20, 20, 50, 100);
+  context.rect(70, 70, 150, 50);
+  context.rect(220, 20, 50, 100);
+  context.rect(20, 170, 50, 50);
+  context.rect(120, 170, 50, 100);
+  context.rect(220, 170, 50, 50);
+  context.stroke();
+  context.fill();
+}
